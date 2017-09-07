9e99497d2dad8d41381ecf2139201c19d37f7089
caro-lang <31735486+caro-lang@users.noreply.github.com>
2017-09-07 18:58:40 +0200

    Add files via upload
    chat-example only showing the first sent message (next step to buzzer function)

---
+++ gameserver.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Caro on 07.09.17.
+ */
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var first=0;
+var safemsg;
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/client.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+    first +=1;
+
+        if (first == 1 ) {
+            safemsg = msg;
+            io.emit('chat message', msg);
+            console.log('message: ' + first);
+            console.log('message: ' + safemsg);
+        }
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+
