995fa109014b5d73056f86a169acbecc9bae33b4
Caro Lang <c-lang@plblang.de>
2017-09-12 19:00:56 +0200

    Effekte versucht zu überarbeiten und Gamemaster bearbeitet

--- gameserver.js
+++ gameserver.js
@@ -10,7 +10,6 @@ var x = 0;
 var erster;
 var playerArray = new Array;
 
-
 //(Gibt für die Demo nur Fehler/Warnungen auf der Konsole aus)
 //var io = require('socket.io').listen(app).set('log level', 1);
 
@@ -22,8 +21,6 @@ app.get('/', function (req, res) {
 app.get('/spass', function (req, res) {
     res.sendFile(__dirname + '/spass.html');
 });
-
-
 
 
 app.get('/overview', function (req, res) {
@@ -61,14 +58,46 @@ io.on('connection', function (client) {
 
     //Clients verbinden sich
     client.on('set nickname', function (nicknameObj) {
+        playerArray.push(nicknameObj);
+        var x = 0 ;
+        console.log("playerArray.length:  "+ playerArray.length);
+        while ( x  < playerArray.length ) {
+            //console.log("playerArray[x].nickname: " + playerArray[x].nickname);
+            console.log("nicknameObj.nickname: " + nicknameObj.nickname);
+            console.log("x: " + x);
+          //  console.log("playerArray.length x: " + playerArray.length[x]);
+            if (playerArray[x].nickname == nicknameObj.nickname) {
+                if (x == 0) {
+                    nicknameObj.color = "green";
+                    nicknameObj.id = x;
+                } else if (x == 1) {
+                    nicknameObj.color = "red";
+                    nicknameObj.id = x;
+                } else if (x == 2) {
+                    nicknameObj.color = "blue";
+                    nicknameObj.id = x;
+                } else if (x == 3) {
+                    nicknameObj.color = "yellow";
+                    nicknameObj.id = x;
+                } else if (x == 4) {
+                    nicknameObj.color = "purple";
+                    nicknameObj.id = x;
+                } else {
+                    console.log("Kritischer Fehler!")
+                }
+                console.log("nicknameobj: " + nicknameObj.color + "nicknameObj.id " + nicknameObj.id );
+            }
+            x++
+        }
 
         client.nicknameObj = nicknameObj;
         //  playerArray.push(nicknameObj);
         console.log(nicknameObj.nickname + " just connected!");
+        //Blaue Felder im Overview füllen und gamemaster Spielerliste erstellen
         io.emit('playerList', nicknameObj);
     });
 
-    //schnellster Buzzer wird ermittelt und an die CLients geschickt
+//schnellster Buzzer wird ermittelt und an die CLients geschickt
     client.on('send_buzz', function (buzzer) {
         console.log("test2 " + buzzer);
         //var obj2 = ({ "name": $('#nameField').val(),  "pkte": 0});
@@ -87,10 +116,10 @@ io.on('connection', function (client) {
     });
 
     /*client.on('questionStart', function (questionArray) {
-        console.log("questionStart");
-        io.emit('questionStart',questionArray);
-    });
-*/
+     console.log("questionStart");
+     io.emit('questionStart',questionArray);
+     });
+     */
     client.on('trueAnswer', function (schnellster) {
 
         console.log(schnellster.nickname + " kriegt Punkte!!");
@@ -103,14 +132,14 @@ io.on('connection', function (client) {
         io.emit('wrongAnswer', schnellster);
     });
 
-    client.on('showQuestion', function (questionArray, tdId) {
-        console.log("sowquestion funzt.pkte: ");
-        io.emit('showQuestion', questionArray, tdId);
+    client.on('showQuestion', function (questionArray, tdId, indx) {
+        console.log("sowquestion funzt.pkte: " + questionArray[indx] + " , " + tdId);
+        io.emit('showQuestion', questionArray, tdId, indx);
     });
 
 
-
-});
+})
+;
 
 
 http.listen(3000, function () {
